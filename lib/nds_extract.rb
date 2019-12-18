7f2869dd021f060d38c7ca8076148bdc5ac45770
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-18 23:27:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,6 +7,7 @@ def directors_totals(nds)
   total_hash = {}
   name_index = 0
   while name_index < nds.length do
+    total_ha
     gross_index = 0
     while gross_index < nds[name_index][:movies].length
      nds[name_index][:movies][gross_index][:worldwide_gross]
