0f4cb942f6e2dee423cec500d11e5e45d6b00aab
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-18 23:35:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ def directors_totals(nds)
     gross_index = 0
     while gross_index < nds[name_index][:movies].length do
      if total_hash[nds[name_index][:name]]
-       
+       total_hash[nds[name_index][:name]] += nds[name_index][:movies][gross_index][]
     gross_index += 1
 end
 end
